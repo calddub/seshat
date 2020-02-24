a909cde9db288be6a672abf7e7cb7de01d72f027
calddub <calobodude@gmail.com>
2020-02-23 20:03:06 -0600

    New comment

--- src/dualcam.py
+++ src/dualcam.py
@@ -7,6 +7,8 @@ from datetime import datetime
 from threading import Thread
 import logging
 
+#impressive changes with tons of typos
+
 #logging.basicConfig(filename='seshat.log',level=logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
 
@@ -15,14 +17,15 @@ class VidCap(Thread):
 	# Tentatively - allow for reading from either webcam or files
 	#   Allow framerate override (especially if working from files)
 	#   Candidate parameters for vidcap object: capheight, capwidth, capframerate
-	#      frambuffercnt, imagesrc (webcam/file), webcam#?, ???
+	#      frambuffercnt, imagesrc (webcam/file), webcam#?, rotation,???
 	#   Should we imbed a storage function to preserve the capture to file
 	#      for later use and recreation / debugging purposes? VidCap allows source
 	#      direct from webcams OR from files so could use it for predictable testing
+	#   For frame buffers, allocate max framebuffer count (10?) and then keep curidx
+	#      value that represents the current frame count captured (could be REALLY high)
+	#      curidx % maxframecnt = index within the buffer of stored frames
 	
-	#def __init__(self):
 	def __init__(self,src,ht,wd,fps):
-		# Body
 		logging.info( "Initializing VidCap Object" )
 		# If src is int (0 or 1 basically) then initialize as webcam, otherwise usr src as filename
 		if( isinstance( src, int )):  # This is webcam number
@@ -36,11 +39,12 @@ class VidCap(Thread):
 
 		self.capheight = ht
 		self.capwidth = wd
+		self.rotation = 0
 
 		#self.vidfeed.set(cv2.CAP_PROP_FRAME_WIDTH,wd)
 		self.vidfeed.set(cv2.CAP_PROP_FRAME_WIDTH,wd)
-		#self.vidfeed.set(cv2.CAP_PROP_FRAME_HEIGHT,ht)
-		#self.vidfeed.set(cv2.CAP_PROP_FPS,fps)
+		self.vidfeed.set(cv2.CAP_PROP_FRAME_HEIGHT,ht)
+		self.vidfeed.set(cv2.CAP_PROP_FPS,fps)
 		self.running = False
 		self.warm = False
 		
@@ -68,12 +72,19 @@ class VidCap(Thread):
 		logging.info( "Running VidCap" )
 		if( self.warm == False ):
 			time.sleep(2)
+			self.warm == True
 
 		while(self.running):
-			logging.debug( "Capture frame" )
 			err,frame = self.vidfeed.read()
+			logging.debug( "Capture frame:"+str(frame.shape) )
+			# Not sure we want to rotate = 2inches additional cam separateion worth 15% CPU overhead?
+			if( self.rotation > 0 ):
+				image_center = tuple(np.array(frame.shape[1::-1]) / 2)
+				rot_mat = cv2.getRotationMatrix2D(image_center, self.rotation, 1.0)
+				result = cv2.warpAffine(frame, rot_mat, frame.shape[1::-1], flags=cv2.INTER_LINEAR)
+				frame = result
 			# temporary throtrtle
-			time.sleep(1/10) #100ms
+			#time.sleep(1/10) #100ms
 
 
 # Additional classes???
@@ -91,6 +102,8 @@ def getts():
 
 vcR = VidCap(0,640,480,30)
 vcL = VidCap(2,640,480,30)
+vcL.rotation = 180
+
 #vc2 = VidCap(2,640,480)
 #vc3 = VidCap(3,640,480)
 vcR.start()
