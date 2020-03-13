8f1bd53a29ad74046c2bc4dded6423d2f3bd3ed0
calddub <calobodude@gmail.com>
2020-03-12 23:26:28 -0500

    Throttling each indvidiaul camera at 15FPS

--- src/dualcam.py
+++ src/dualcam.py
@@ -25,8 +25,8 @@ class VidCap(Thread):
 	#      value that represents the current frame count captured (could be REALLY high)
 	#      curidx % maxframecnt = index within the buffer of stored frames
 	
-	def __init__(self,src,ht,wd,fps):
-		logging.info( "Initializing VidCap Object" )
+	def __init__(self,src,nm,ht,wd,fps):
+		logging.info( "Initializing VidCap Object"+nm )
 		# If src is int (0 or 1 basically) then initialize as webcam, otherwise usr src as filename
 		if( isinstance( src, int )):  # This is webcam number
 			self.vidfeed = cv2.VideoCapture(src)
@@ -41,6 +41,7 @@ class VidCap(Thread):
 		self.capwidth = wd
 		self.rotation = 0
 		self.framecnt = 0
+		self.camname = nm
 
 		#self.vidfeed.set(cv2.CAP_PROP_FRAME_WIDTH,wd)
 		self.vidfeed.set(cv2.CAP_PROP_FRAME_WIDTH,wd)
@@ -76,34 +77,41 @@ class VidCap(Thread):
 			self.warm == True
 
 		while(self.running):
-			err,frame = self.vidfeed.read()
-			# Not sure we want to rotate = 2inches additional cam separateion worth 15% CPU overhead?
-			if( self.rotation > 0 ):
-				image_center = tuple(np.array(frame.shape[1::-1]) / 2)
-				rot_mat = cv2.getRotationMatrix2D(image_center, self.rotation, 1.0)
-				result = cv2.warpAffine(frame, rot_mat, frame.shape[1::-1], flags=cv2.INTER_LINEAR)
-				frame = result
-			# temporary throtrtle
-			#time.sleep(1/10) #100ms
-
-			# Temporary conversion steps
-			# Leverage gray scale for our image comparison
-			gsframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-			# Blur/fuzz the image a bit to eliminate background noise
-			blurframe = cv2.GaussianBlur(gsframe, (7, 7), 0)
-
-			diff = cv2.absdiff(blurframe.astype("uint8"), blurframe)
-
-			# Thresholding logic to evaluate the difference between before image and after image
-			thresh1 = cv2.threshold(diff, 0.5*100, 255, cv2.THRESH_BINARY)[1]
-			thresh2 = cv2.erode(thresh1, None, iterations=2)
-			thresh3 = cv2.dilate(thresh2, None, iterations=2)
-
-			# Pull the contours / differences between frames. Theorietically minimal change == 0
-			contours = cv2.findContours(thresh3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+			succ,frame = self.vidfeed.read()
 			self.framecnt += 1
-			logging.debug( "Capture frame:"+str(self.framecnt) +" "+str(frame.shape) )
+
+			if not succ:
+				logging.debug( "Capture frame("+self.camname+"):"+str(self.framecnt) +" err:"+str(succ) )
+			else:
+				logging.debug( "Capture frame("+self.camname+"):"+str(self.framecnt) +" "+str(frame.shape)+" success:"+str(succ) )
+
+				# Not sure we want to rotate = 2inches additional cam separation worth 10% CPU overhead?
+				if( self.rotation > 0 ):
+					image_center = tuple(np.array(frame.shape[1::-1]) / 2)
+					rot_mat = cv2.getRotationMatrix2D(image_center, self.rotation, 1.0)
+					result = cv2.warpAffine(frame, rot_mat, frame.shape[1::-1], flags=cv2.INTER_LINEAR)
+					frame = result
+				# temporary throttle
+				#time.sleep(1/10) #100ms
+
+
+
+				# Temporary conversion steps
+				# Leverage gray scale for our image comparison
+				gsframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				# Blur/fuzz the image a bit to eliminate background noise
+				blurframe = cv2.GaussianBlur(gsframe, (7, 7), 0)
+
+				diff = cv2.absdiff(blurframe.astype("uint8"), blurframe)
+
+				# Thresholding logic to evaluate the difference between before image and after image
+				thresh1 = cv2.threshold(diff, 0.5*100, 255, cv2.THRESH_BINARY)[1]
+				thresh2 = cv2.erode(thresh1, None, iterations=2)
+				thresh3 = cv2.dilate(thresh2, None, iterations=2)
+
+				# Pull the contours / differences between frames. Theorietically minimal change == 0
+				contours = cv2.findContours(thresh3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
 
 
 
@@ -120,8 +128,11 @@ def getts():
 	return ts, strts
 
 
-vcR = VidCap(0,640,480,30)
-vcL = VidCap(2,640,480,30)
+#vcR = VidCap(0,"RCam",640,480,30)
+vcR = VidCap(0,"RCam",640,480,15)
+vcR.rotation = 180
+#vcL = VidCap(2,"LCam",640,480,30)
+vcL = VidCap(2,"LCam",640,480,15)
 vcL.rotation = 180
 
 #vc2 = VidCap(2,640,480)
