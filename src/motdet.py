e18e8ededdf149ba0ba267e5c9e9b10a75beb190
calddub <calobodude@gmail.com>
2020-04-26 13:54:05 -0500

    Continued refinement of motdet object

--- src/motdet.py
+++ src/motdet.py
@@ -11,8 +11,6 @@ from vidwrtr import VidWrtr
 #logging.basicConfig(filename='seshat.log',level=logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
 
-
-
 # MotionDetector Object
 #   Variables
 #     HistoryCnt
@@ -41,99 +39,65 @@ class MotDet():
 	#      value that represents the current frame count captured (could be REALLY high)
 	#      curidx % maxframecnt = index within the buffer of stored frames
 	
-	def __init__(self,src,nm,ht,wd,fps):
-		logging.info( "Initializing VidCap Object"+nm )
-		# If src is int (0 or 1 basically) then initialize as webcam, otherwise usr src as filename
-		if( isinstance( src, int )):  # This is webcam number
-			self.vidfeed = cv2.VideoCapture(src)
-			self.vidsrc = src
-			self.webcam = True
-		else:
-			self.vidfeed = cv2.VideoCapture(src)
-			self.vidsrc = src
-			self.webcam = False
+	def __init__(self,nm,ht,wd,sz):
+		logging.info( "Initializing MoteionDetector Object"+nm )
 
-		self.capheight = ht
-		self.capwidth = wd
-		self.rotation = 0
-		self.framecnt = 0
-		self.camname = nm
+		self.frameht = ht
+		self.framewd = wd
+		self.detname = nm
+		self.queuesz = sz
 
-		#self.vidfeed.set(cv2.CAP_PROP_FRAME_WIDTH,wd)
-		self.vidfeed.set(cv2.CAP_PROP_FRAME_WIDTH,wd)
-		self.vidfeed.set(cv2.CAP_PROP_FRAME_HEIGHT,ht)
-		self.vidfeed.set(cv2.CAP_PROP_FPS,fps)
-		self.running = False
-		self.warm = False
-		
-		self.savefeed = False
+		# Standard frame info
+		self.framecnt = -1    # Will ++ on each newframe so start with negative framecnt index
+		self.rawframe = []    # Raw frame data direct from cam/source
+		self.bwframe = []     # Frame translated into BW data
+		self.blurframe = []   # Frame after running through initial Gaussian Blur
+
+		# Motion Detection Info
+		self.diffframe = []   # Diff from previous frame (if applicable)
+		self.threshframe = [] # Threadshold calc from based on diff
+		self.erodeframe = []  # Erosion process to reduce noise
+		self.dilateframe = [] # Dilation expands on detected deltas
+		self.countours = []   # Calculated contours for motion within frame
+
 
 		ts, strts = getts()
-		logging.debug( "Timestamp:"+strts )
-		logging.debug( "Capture height="+str(self.capheight)+" width="+str(self.capwidth ))
-
-		## TODO: TEMPORARY!
-		#self.vidout = VidWrtr(nm+".mov",640,480,15)
-		self.vidout = VidWrtr(nm,640,480,15)
-
-		Thread.__init__(self)
-
-	def start(self):
-		# Body
-		logging.info( "Start VidCap" )
-		self.running = True
-		self.vidout.start()
-		Thread.start(self)
+		logging.debug( "MotionDetector initialized - Timestamp:"+strts )
 
 
-	def stop(self):
-		# Body
-		logging.info( "Stopping VidCap" )
-		self.vidout.stop()
-		self.running = False
+	def addFrame(self,frame):
+		# Temporary conversion steps
+		refidx = self.framecnt++%self.queuesz
 
-	def run(self):
-		# Body
-		logging.info( "Running VidCap" )
-		if( self.warm == False ):
-			time.sleep(2)
-			self.warm == True
+		# Early in the process will need to allocate data so it can be overwritten later on
+		if( self.framecnt < self.queuesz ):
+			
+			self.rawframe.append(frame)
+			self.bwframe.append(frame)
+			self.blurframe.append(frame)
+			self.diffframe.append(frame)
+			self.threshframe.append(frame)
+			self.erodeframe.append(frame)
+			self.dilateframe.append(frame)
 
-		while(self.running):
-			succ,frame = self.vidfeed.read()
-			self.framecnt += 1
+		# Leverage gray scale for our image comparison
+		self.bwframe[refidx] = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		# Blur/fuzz the image a bit to eliminate background noise
+		self.blurframe[refidx] = cv2.GaussianBlur(self.bwframe[refidx], (7, 7), 0)
 
-			if not succ:
-				logging.debug( "Capture frame("+self.camname+"):"+str(self.framecnt) +" err:"+str(succ) )
-			else:
-				logging.debug( "Capture frame("+self.camname+"):"+str(self.framecnt) +" "+str(frame.shape)+" success:"+str(succ) )
+		diff = cv2.absdiff(blurframe.astype("uint8"), blurframe)
 
-				### TEMPORARY
-				self.vidout.write(frame)
+		# Thresholding logic to evaluate the difference between before image and after image
+		thresh1 = cv2.threshold(diff, 0.5*100, 255, cv2.THRESH_BINARY)[1]
+		thresh2 = cv2.erode(thresh1, None, iterations=2)
+		thresh3 = cv2.dilate(thresh2, None, iterations=2)
 
-				# Not sure we want to rotate = 2inches additional cam separation worth 10% CPU overhead?
-				if( self.rotation > 0 ):
-					image_center = tuple(np.array(frame.shape[1::-1]) / 2)
-					rot_mat = cv2.getRotationMatrix2D(image_center, self.rotation, 1.0)
-					result = cv2.warpAffine(frame, rot_mat, frame.shape[1::-1], flags=cv2.INTER_LINEAR)
-					frame = result
-				# temporary throttle
-				#time.sleep(1/10) #100ms
+	def calcDiff(self,frame1,frame2):
+		pass
 
+	def getContours(self):
+		pass
 
-				# Temporary conversion steps
-				# Leverage gray scale for our image comparison
-				gsframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-				# Blur/fuzz the image a bit to eliminate background noise
-				blurframe = cv2.GaussianBlur(gsframe, (7, 7), 0)
-
-				diff = cv2.absdiff(blurframe.astype("uint8"), blurframe)
-
-				# Thresholding logic to evaluate the difference between before image and after image
-				thresh1 = cv2.threshold(diff, 0.5*100, 255, cv2.THRESH_BINARY)[1]
-				thresh2 = cv2.erode(thresh1, None, iterations=2)
-				thresh3 = cv2.dilate(thresh2, None, iterations=2)
-
-				# Pull the contours / differences between frames. Theorietically minimal change == 0
-				contours = cv2.findContours(thresh3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	def getContoursByID(self,frameid):
+		pass
 
